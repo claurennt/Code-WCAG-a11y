e1315fe1ac67512cbeb8cd537c2656cb85cb10d1
claurennt <c.benedetti1989@gmail.com>
2026-02-04 15:02:08 +0100

    refactor: add function annotations, improve logging

--- code_wcag_a11y/scripts/chromadb.py
+++ code_wcag_a11y/scripts/chromadb.py
@@ -1,6 +1,7 @@
 import chromadb
 from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
 
+from code_wcag_a11y import client
 from code_wcag_a11y.globals import CHROMADB_WCAG_PATH, COLLECTION_NAME
 
 
@@ -12,9 +13,10 @@ def get_embedding_model():
     return SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
 
 
-client = get_vector_client(CHROMADB_WCAG_PATH)
-embedding_function = get_embedding_model()
-
-collection = client.get_or_create_collection(
-    name=COLLECTION_NAME, embedding_function=embedding_function
-)
+def get_collection():
+    client = get_vector_client(CHROMADB_WCAG_PATH)
+    embedding_function = get_embedding_model()
+    collection = client.get_or_create_collection(
+        name=COLLECTION_NAME, embedding_function=embedding_function
+    )
+    return collection
